a1c37901a9af475a6c5d475401596742ff00161b
Javid Asgarov <asgarov1@gmail.com>
2023-07-09 23:51:37 +0200

    reimplemented the functionality with ajax

--- src/main/java/com/asgarov/wicket/demo/counter/MyCounter.java
+++ src/main/java/com/asgarov/wicket/demo/counter/MyCounter.java
@@ -1,20 +1,27 @@
 package com.asgarov.wicket.demo.counter;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxFallbackLink;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
 
-public class MyCounter extends WebPage {
+import java.util.Optional;
 
+public class MyCounter extends WebPage {
     int counter = 0;
 
     public MyCounter() {
-        add(new Link<Void>("counter-link") {
+        Label label = new Label("counter-label", () -> counter);
+        label.setOutputMarkupId(true);
+        add(label);
+
+        add(new AjaxFallbackLink<>("counter-link") {
             @Override
-            public void onClick() {
+            public void onClick(Optional<AjaxRequestTarget> target) {
                 counter++;
+                target.ifPresent(t -> t.add(label));
             }
         });
-        add(new Label("counter-label", () -> counter));
     }
 }
