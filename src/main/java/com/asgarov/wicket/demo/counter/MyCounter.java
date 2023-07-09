578da51086ed60e538aa6f6507e3cc8be41123bc
Javid Asgarov <asgarov1@gmail.com>
2023-07-09 23:27:35 +0200

    refactoring

--- src/main/java/com/asgarov/wicket/demo/counter/MyCounter.java
+++ src/main/java/com/asgarov/wicket/demo/counter/MyCounter.java
@@ -4,12 +4,6 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
 
-
-/**
- * Page with examples on {@link org.apache.wicket.markup.html.link.Link}.
- *
- * @author Eelco Hillenius
- */
 public class MyCounter extends WebPage {
 
     int counter = 0;
